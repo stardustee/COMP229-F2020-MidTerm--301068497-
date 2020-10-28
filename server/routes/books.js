55a2574881136e155662049168dc3be6c549dcfa
sydney <sydneyhuang@rogers.com>
2020-10-28 17:57:31 -0400

    Fixed

--- server/routes/books.js
+++ server/routes/books.js
@@ -4,21 +4,21 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 // define the book model
-let Books = require('../models/books');
+let book = require('../models/books');
 
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  books.find( (err, books) => {
+  book.find( (err, books) => {
     if (err) {
       return console.error(err);
     }
-    else 
-    {
-      console.log(data);
-      res.render('/books.ejs', { title: 'Books', books: data });
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
     }
-
   });
 
 });
@@ -30,8 +30,18 @@ router.get('/add', (req, res, next) => {
      * ADD CODE HERE *
      *****************/
 
-    res.render('index', { title: 'Add Book' });
- /*****************************************************/
+    Books.find( (err, books) => {
+      if (err) {
+        return console.error(err);
+      }
+      else 
+      {
+        console.log(books);
+        res.render('/books', { title: 'Books', books: books });
+      }
+  
+    });
+
 });
 
 // POST process the Book Details page and create a new Book - CREATE
@@ -40,23 +50,6 @@ router.post('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-    let books = Books.Model({
-      "title" :  req.body.name,
-      "description" :  req.body.name,
-      "price" :  req.body.name,
-      "author" :  req.body.name,
-      "genre":  req.body.name
-  });
-
-  Book.Model.create(books, (err, Books) => {
-      if(err)
-      {
-          console.log(err);
-          res.end(err);
-      }
-      res.redirect('/index');
-  });
-  /*****************************************************/
 
 });
 
@@ -66,19 +59,6 @@ router.get('/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-    let id = req.params.id;
-
-    // pass id to the db 
-    Books.Model.findById(id, (err, BooksToEdit) => {
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
-        }
-        // show the edit view
-        res.render('/index', { title: 'Edit book', data });
-    });
- /*****************************************************/
 });
 
 // POST - process the information passed from the details form and update the document
@@ -88,26 +68,6 @@ router.post('/:id', (req, res, next) => {
      * ADD CODE HERE *
      *****************/
 
-    let id = req.params.id;
-
-    // instantiate a new object of type Component
-    let updatedBooks = Books.Model({
-      "title" :  req.body.name,
-      "description" :  req.body.name,
-      "price" :  req.body.name,
-      "author" :  req.body.name,
-      "genre":  req.body.name
-   });
-
-   Books.Model.updateOne({_id: id}, updatedBooks, (err) => {
-       if(err)
-       {
-           console.log(err);
-           res.end(err);
-       }
-       res.redirect('/index');
-   });
-
 });
 
 // GET - process the delete by user id
@@ -116,16 +76,6 @@ router.get('/delete/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-    let id = req.params.id;
-
-    Books.Model.remove({_id: id}, (err) => {
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
-        }
-        res.redirect('/index');
-    });
 });
 
 
